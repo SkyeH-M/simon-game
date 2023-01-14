757e78be22902adf883f4d3e963e7e3d2e3ccbde
SkyeH-M <justskye98@gmail.com>
2023-01-14 16:51:52 +0000

    Tests failing

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,15 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: [],
-}
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+};
 
 // export game object into test file
-module.exports = { game };
+module.exports = { game, newGame };
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,8 @@
  */
 
 // import game obj into test file:
-const { game } = require("../game");
+const { game, newGame } = require("../game");
+
 
 beforeAll(() => {
     // this code will be the same for any html file to load into the DOM
@@ -26,5 +27,26 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        newGame();
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
     });
 });
